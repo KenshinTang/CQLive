11aca7f087a0af2e87af59f5645026fcbb27819f
KenshinTang <28135055@qq.com>
2019-09-03 12:14:07 +0800

    initial xul module

---
+++ app/src/main/java/com/kapplication/cqlive/KApplication.kt
@@ -0,0 +1,52 @@
+package com.kapplication.cqlive
+
+import com.kapplication.cqlive.message.CommonMessage
+import com.starcor.xulapp.XulApplication
+import com.starcor.xulapp.message.XulMessageCenter
+import com.starcor.xulapp.utils.XulLog
+
+class KApplication : XulApplication() {
+
+    private val XUL_FIRST_PAGE = "xul_layouts/pages/xul_epg_page.xml"
+    private val XUL_GLOBAL_BINDINGS = "xul_layouts/xul_global_bindings.xml"
+    private val XUL_GLOBAL_SELECTORS = "xul_layouts/xul_global_selectors.xml"
+    private val XUL_GLOBAL_DIALOGS = "xul_layouts/pages/xul_global_dialogs.xml"
+
+    override fun onCreate() {
+        XulLog.i("CQLive", "KApplication onCreate.")
+
+        super.onCreate()
+        startCommonMessage()
+    }
+
+    override fun onLoadXul() {
+        xulLoadLayouts(XUL_FIRST_PAGE)
+        xulLoadLayouts(XUL_GLOBAL_BINDINGS)
+        xulLoadLayouts(XUL_GLOBAL_SELECTORS)
+        xulLoadLayouts(XUL_GLOBAL_DIALOGS)
+        super.onLoadXul()
+    }
+
+    private fun startCommonMessage() {
+        XulMessageCenter.buildMessage()
+            .setTag(CommonMessage.EVENT_HALF_SECOND)
+            .setInterval(500)
+            .setRepeat(Integer.MAX_VALUE)
+            .postSticky()
+
+        XulMessageCenter.buildMessage()
+            .setTag(CommonMessage.EVENT_TEN_MINUTES)
+            .setInterval(1000 * 60 * 10)
+            .setRepeat(Integer.MAX_VALUE)
+            .setDelay(1000 * 60 * 1)
+            .postSticky()
+
+        XulMessageCenter.buildMessage()
+            .setTag(CommonMessage.EVENT_HALF_HOUR)
+            .setInterval(1000 * 60 * 30)
+//                .setInterval(1000 * 10)
+            .setRepeat(Integer.MAX_VALUE)
+            .setDelay(1000 * 60 * 30)
+            .postSticky()
+    }
+}
---
+++ app/src/main/java/com/kapplication/cqlive/message/CommonMessage.kt
@@ -0,0 +1,37 @@
+package com.kapplication.cqlive.message
+
+object CommonMessage {
+    private const val EVENT_ID_BASE = 0x4000
+    private const val EVENT_CAT_BASE = 0x100000
+    @Volatile private var currentBaseMsgId = EVENT_ID_BASE
+    @Volatile private var currentCatBase = EVENT_CAT_BASE * 0x20
+
+    const val CAT_ACTIVITY = EVENT_CAT_BASE * 0x01
+    const val EVENT_ACTIVITY_CREATED = CAT_ACTIVITY + 0x001
+    const val EVENT_ACTIVITY_RESUMED = CAT_ACTIVITY + 0x002
+    const val EVENT_ACTIVITY_STOPPED = CAT_ACTIVITY + 0x003
+    const val EVENT_ACTIVITY_DESTROYED = CAT_ACTIVITY + 0x004
+
+    const val EVENT_HALF_SECOND = 0x1001
+    const val EVENT_HALF_HOUR = 0x1002
+    const val EVENT_TEN_MINUTES = 0x1003
+
+    const val EVENT_SHOW_UPGRADE = 0x2001
+
+    @Synchronized
+    fun GENERAGE_UNIQUE_MSG_ID(): Int {
+        return ++currentBaseMsgId
+    }
+
+    @Synchronized
+    fun GENERAGE_UNIQUE_MSG_ID(catId: Int): Int {
+        return ++currentBaseMsgId + catId
+    }
+
+    @Synchronized
+    fun GENERAGE_UNIQUE_CAT_ID(): Int {
+        currentCatBase += EVENT_CAT_BASE
+        return currentCatBase
+    }
+
+}
